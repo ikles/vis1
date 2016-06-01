0b621d7d56a25970acf59b66cbf22724901977c5
Igor Kleshev <membship@ya.ru>
2016-06-01 23:33:42 +0700

    start responsive

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,6 @@
 $(function() {
 
-var glide = $('.slider').glide().data('api_glide');
+	var glide = $('.slider').glide().data('api_glide');
 	$(window).on('keyup', function (key) {
 		if (key.keyCode === 13) {
 			glide.jump(3, console.log('Wooo!'));
@@ -11,9 +11,23 @@ var glide = $('.slider').glide().data('api_glide');
 		console.log(glide.current());
 	});
 
-$('.slider').glide({
-	arrows: false,
-	autoplay: 5000
+	$('.slider').glide({
+		arrows: false,
+		autoplay: 5000
 	});
+
+	$('.all_tags').click(function (b) {
+		b.preventDefault();
+		$('.tags').css('height','auto');
+		$(this).hide();
+		$('.hide_tags').show();	
+	});
+
+	$('.hide_tags').click(function (b) {
+		b.preventDefault();
+		$('.tags').css('height','50px');
+		$(this).hide();
+		$('.all_tags').show();
+	})
 
 });
